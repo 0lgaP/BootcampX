0181bfcc29849d030388e1f55a481bd65b4d6d43
0lgaP <perronolga@gmail.com>
2022-02-08 14:51:03 +0000

    teachers and assistance_requests tables are added to database and seeded

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -34,12 +34,5 @@ CREATE TABLE assignment_submissions (
 
 -- );
 
-SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
-FROM students
-JOIN assignment_submissions ON assignment_submissions.student_id = students.id
-JOIN assignments ON assignments.id = assignment_submissions.assignment_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-ORDER BY average_assignment_duration;
 
 
